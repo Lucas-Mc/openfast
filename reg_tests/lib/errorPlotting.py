3c892d5abb64f8186ef148911199abe996f53d14
Rafael M Mudafort <rafmudaf@gmail.com>
2019-08-20 11:14:01 -0600

    Bug fix: Use page title as given in the arguments

--- reg_tests/lib/errorPlotting.py
+++ reg_tests/lib/errorPlotting.py
@@ -132,7 +132,7 @@ def _htmlHead(title):
     head  = '<!DOCTYPE html>' + '\n'
     head += '<html>' + '\n'
     head += '<head>' + '\n'
-    head += '  <title>5MW_ITIBarge_DLL_WTurb_WavesIrr Summary</title>' + '\n'
+    head += '  <title>{}</title>'.format(title) + '\n'
     head += '  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>' + '\n'
     head += '  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>' + '\n'
     head += '  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>' + '\n'
